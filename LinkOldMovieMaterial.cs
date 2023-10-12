401b6b4243b01e1626923185fa47db2638b7a1f1
JustNicheng <137873909+JustNicheng@users.noreply.github.com>
2023-10-12 17:39:51 +0800

    Initial commit

---
+++ LinkOldMovieMaterial.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+
+public class LinkOldMovieMaterial : MonoBehaviour
+{
+    #region 載入與更新
+    [SerializeField, Header("連結的Material")]
+    private Material _linkMaterial;
+    [SerializeField, Header("划痕")]
+    private ComputeShader _scratchesShader;
+    [SerializeField, Header("汙點")]
+    private ComputeShader _dustShader;
+    [SerializeField, Header("外圈陰影")]
+    private ComputeShader _outermostShader;
+    RenderTexture[] _allRenderTex;
+    private void Start()
+    {
+        _allRenderTex = new RenderTexture[] { new RenderTexture(1024, 1024, 24), new RenderTexture(1024, 1024, 24), new RenderTexture(1024, 1024, 24) };
+        LinkAllMaterial();
+        _nowTimePerAni = 1.0f / theFPS;
+    }
+    [SerializeField,Header("老電影效果閃爍FPS")]
+    private float theFPS;
+    private float _nowTimePerAni;
+    void Update()
+    {
+        _nowTimePerAni -= Time.deltaTime;
+        if (_nowTimePerAni <= 0)
+        {
+            _nowTimePerAni += 1.0f / theFPS;
+            UpdateTex();
+        }
+    }
+    void LinkAllMaterial()
+    {
+        for (int i = 0; i < _allRenderTex.Length; i++)
+        {
+            _allRenderTex[i].enableRandomWrite = true;
+            _allRenderTex[i].Create();
+            switch (i)
+            {
+                case 0:
+                    _linkMaterial.SetTexture("_ScratchTex", _allRenderTex[i]);
+                    ScratchTextureUpdate(ref _allRenderTex[i]);
+                    break;
+                case 1:
+                    _linkMaterial.SetTexture("_DustTex", _allRenderTex[i]);
+                    DustTextureUpdate(ref _allRenderTex[i]);
+                    break;
+                case 2:
+                    _linkMaterial.SetTexture("_OutermostShader", _allRenderTex[i]);
+                    OutermostTextureUpdate(ref _allRenderTex[i]);
+                    break;
+            }
+
+
+        }
+    }
+    void UpdateTex()
+    {
+        for (int i = 0; i < _allRenderTex.Length; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    ScratchTextureUpdate(ref _allRenderTex[i]);
+                    break;
+                case 1:
+                    DustTextureUpdate(ref _allRenderTex[i]);
+                    break;
+                case 2:
+                    OutermostTextureUpdate(ref _allRenderTex[i]);
+                    break;
+            }
+
+
+        }
+    }
+    #endregion
+    #region Scratch
+    [SerializeField, Header("劃痕寬上限"), Range(0.1f, 30f)]
+    float scratchWidthMax;
+    [SerializeField, Header("劃痕數量上限"), Range(1, 100)]
+    int scratchCountMax;
+    void ScratchTextureUpdate(ref RenderTexture _rt)
+    {
+
+        int kernelIndex = _scratchesShader.FindKernel("CSMain");
+        int randomLen = Random.Range(1, scratchCountMax);//1~30條劃痕
+        _scratchesShader.SetInt("posArrayaLength", randomLen);
+        ComputeBuffer _uniformArray = new ComputeBuffer(randomLen, sizeof(float) * 3);
+        _uniformArray.SetData(RandomScratch(randomLen));
+        _scratchesShader.SetBuffer(kernelIndex, "RandomPosArray", _uniformArray);
+        _scratchesShader.SetTexture(kernelIndex, "ScratchesTex", _rt);
+        _scratchesShader.Dispatch(kernelIndex, 1024 / 8, 1024 / 8, 1);
+        _uniformArray.Release();
+    }
+    Vector3[] RandomScratch(int randomLen)
+    {
+        Vector3[] _randomScratches = new Vector3[randomLen];
+        for (int i = 0; i < randomLen; i++)
+        {
+            _randomScratches[i] = new Vector3(Random.Range(.0f, 1024f),//X座標
+                                              0,//不需要Y座標
+                                              Random.Range(.0f, scratchWidthMax));//寬度
+        }
+        return _randomScratches;
+    }
+    #endregion
+    #region Dust
+    [SerializeField, Header("汙點半徑上限"), Range(0.1f, 30f)]
+    float dustRadiusMax;
+    [SerializeField, Header("汙點數量上限"), Range(1, 100)]
+    int dustCountMax;
+    void DustTextureUpdate(ref RenderTexture _rt)
+    {
+        int kernelIndex = _dustShader.FindKernel("CSMain");
+        int randomLen = Random.Range(1, dustCountMax);//隨機數量點點
+        _dustShader.SetInt("posArrayaLength", randomLen);
+        ComputeBuffer _uniformArray = new ComputeBuffer(randomLen, sizeof(float) * 3);
+        _uniformArray.SetData(RandomDust(randomLen));
+        _dustShader.SetBuffer(kernelIndex, "RandomPosArray", _uniformArray);
+        _dustShader.SetTexture(kernelIndex, "DustTex", _rt);
+        _dustShader.Dispatch(kernelIndex, 1024 / 8, 1024 / 8, 1);
+        _uniformArray.Release();
+    }
+    Vector3[] RandomDust(int randomLen)
+    {
+        Vector3[] _randomScratches = new Vector3[randomLen];
+        for (int i = 0; i < randomLen; i++)
+        {
+            _randomScratches[i] = new Vector3(Random.Range(.0f, 1024f),//X座標
+                                              Random.Range(.0f, 1024f),//Y座標
+                                              Random.Range(.0f, dustRadiusMax));//半徑
+        }
+        return _randomScratches;
+    }
+    #endregion
+    #region Outermost
+    [SerializeField, Header("最外圈的範圍")]
+    Vector2 blackWidthRange;
+    [SerializeField, Header("最外圈到中間空白之間過度的範圍")]
+    Vector2 gradientWidthRange;
+
+    void OutermostTextureUpdate(ref RenderTexture _rt)
+    {
+        int kernelIndex = _outermostShader.FindKernel("CSMain");
+        int blackWidth = (int)Random.Range(blackWidthRange.x, blackWidthRange.y);
+        int gradientWidth = (int)Random.Range(gradientWidthRange.x, gradientWidthRange.y);
+        _outermostShader.SetInt("BlackWidth", blackWidth);
+        _outermostShader.SetInt("GradientWidth", gradientWidth);
+        _outermostShader.SetTexture(kernelIndex, "OutermostTex", _rt);
+        _outermostShader.Dispatch(kernelIndex, 1024 / 8, 1024 / 8, 1);
+    }
+    #endregion
+}
---
+++ OldMovieShader.shader
@@ -0,0 +1,90 @@
+﻿Shader "Custom/OldMovieShader"
+{
+    //Material顯示的內容
+    Properties
+    {
+        //程式內變數(Inspector顯示名稱，類型) = 預設值
+        _MainTex ("原圖", 2D) = "white" {}
+        //老電影效果範例 https://www.youtube.com/watch?v=1hGhgD_VUyM
+        _ScratchTex ("刮痕圖片", 2D) = "white" {}
+        _ScratchEffectValue("刮痕響值",Range(0, 1)) = 0.5
+        _DustTex("灰塵圖片",2D) = "white"{}
+        _DustEffectValue("灰塵響值",Range(0, 1)) = 0.5
+        _OutermostShader("最外層陰影圖",2D) = "white"{}
+        _OutermostEffectValue("最外層響值",Range(0, 1)) = 0.5
+        _OldFilmColor ("老電影遮罩顏色", Color) = (1,1,1,1)
+        _EffectValue("總影響值",Range(0, 1)) = 0.5
+    }
+    SubShader
+    {
+        
+        Tags { "RenderType"="Opaque" }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vertShader
+            #pragma fragment fragShader
+            #include "UnityCG.cginc"
+
+            uniform sampler2D _MainTex;
+            uniform sampler2D _ScratchTex;
+            uniform float _ScratchEffectValue;
+            uniform sampler2D _DustTex;
+            uniform float _DustEffectValue;
+            uniform sampler2D _OutermostShader;
+            uniform float _OutermostEffectValue;
+            uniform float4 _OldFilmColor;
+            uniform float _EffectValue;
+        
+            struct p2v
+            {
+                float4 pos_MainTex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+            struct v2f
+            {
+                float4 pos : SV_POSITION; // 頂點
+                float2 uv : TEXCOORD0; // 頂點
+            };
+            v2f vertShader(p2v v) 
+            {
+                v2f vertRes;
+                vertRes.pos = UnityObjectToClipPos(v.pos_MainTex);
+                vertRes.uv = v.uv; // 頂點換到Clip空間
+                return vertRes;
+            }
+            float4 fragShader(v2f vertRes) : Color
+            {
+                half2 TexUV = vertRes.uv;
+                //取得主圖的顏色
+			    float4 renderTex = tex2D(_MainTex, TexUV);
+                //取得三張遮罩指定位置的顏色
+                float4 scratchTex = tex2D(_ScratchTex, TexUV);
+				
+				float4 dustTex = tex2D(_DustTex, TexUV);
+
+				float4 outermostTex = tex2D(_OutermostShader, TexUV);
+				
+				// 取得亮度YIQ
+				float _YIQ = dot(fixed3(0.299, 0.587, 0.114), renderTex.rgb);
+				
+				float4 finalColor = _YIQ * _OldFilmColor;
+				
+				// Create a constant white color we can use to adjust opacity of effects
+				float3 whiteColor = fixed3(1, 1, 1);
+				
+				// Composite together the different layers to create final Screen Effect
+				finalColor.rgb *= lerp(whiteColor, scratchTex, _ScratchEffectValue);
+				finalColor.rgb *= lerp(whiteColor, dustTex, _DustEffectValue);
+				finalColor.rgb *= lerp(whiteColor, outermostTex, _OutermostEffectValue);
+				finalColor = lerp(finalColor, renderTex, 1.0f-_EffectValue);
+				
+				
+				return finalColor;
+            }
+            ENDCG
+        }
+        
+    }
+    FallBack "Diffuse"
+}
